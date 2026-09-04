a65711e1dd4cdac6839f746092853eed20def28c
agttzx <agttzzx@gmail.com>
2026-09-04 15:19:40 -0300

    g
    g

---
+++ aula 2 de c++/Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int nota = 4;
+	int presenca = 75;
+
+	if (!(nota >= 7 || presenca >=75)){
+	printf ("verdadeiro");
+	}else {
+	printf("falso");
+	}
+	return 0;
+}
